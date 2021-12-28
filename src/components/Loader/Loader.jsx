faa5ad89e6a000be9add2887406169bc0826187e
Samarpan Harit <samarpan.harit@unicommerce.com>
2021-12-28 22:53:31 +0530

    Updated Loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Spinner } from "@chakra-ui/react";
 
 function Loader() {
     return (
-        <div>
-            Loading ...
+        <div className="flex justify-center items-center w-screen h-screen bg-black text-white">
+            <div className="flex justify-center items-center">
+                <div className="text-2xl">Loading</div> <div className="w-4"></div>
+                <Spinner color='green.300'/>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Loader
+export default Loader;

a68e6407c17c872ffe4ad95923aae6bee55c26ac
Samarpan Harit <haritsamarpan7@gmail.com>
2022-12-30 20:03:29 +0530

    Add .env.development file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,1 @@
-console.log(import.meta.env);
-export const urlPrefix = import.meta.env.DEV
-	? import.meta.env.VITE_REACT_APP_BACKEND_TEST_URL
-	: import.meta.env.VITE_REACT_APP_BACKEND_PROD_URL;
+export const urlPrefix = import.meta.env.VITE_REACT_APP_BACKEND_URL;
